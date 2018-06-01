793737bf509fc497f0c15bd9228748be4cc8fa0d
Anton Evdokimov <free0u@gmail.com>
2018-06-01 19:56:51 +0500

    Add ds18b20 reading

--- lib/SensorDallasTemp/SensorDallasTemp.cpp
+++ lib/SensorDallasTemp/SensorDallasTemp.cpp
@@ -1,6 +1,72 @@
 #include "SensorDallasTemp.h"
 #include <HardwareSerial.h>
 
+// function to print a device address
+void printAddress(DeviceAddress deviceAddress)
+{
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    if (deviceAddress[i] < 16) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
+}
+
 SensorDallasTemp::SensorDallasTemp() {
-    Serial.println("SensorDallasTemp created");
+    oneWire = new OneWire(ONE_WIRE_BUS);
+    sensors = new DallasTemperature(oneWire);
+
+    Serial.println("Dallas Temperature IC Control Library Demo");
+    sensors->begin();
+    numberOfDevices = sensors->getDeviceCount();
+    numberOfDevices = 1;
+
+    Serial.print("Locating devices...");
+
+    Serial.print("Found ");
+    Serial.print(numberOfDevices, DEC);
+    Serial.println(" devices.");
+
+    // Loop through each device, print out address
+    for(int i=0;i<numberOfDevices; i++)
+    {
+        // Search the wire for address
+        if(sensors->getAddress(tempDeviceAddress, i))
+        {
+            Serial.print("Found device ");
+            Serial.print(i, DEC);
+            Serial.print(" with address: ");
+            printAddress(tempDeviceAddress);
+            Serial.println();
+            
+            Serial.print("Resolution actually set to: ");
+            Serial.print(sensors->getResolution(tempDeviceAddress), DEC); 
+            Serial.println();
+        }else{
+            Serial.print("Found ghost device at ");
+            Serial.print(i, DEC);
+            Serial.print(" but could not detect address. Check power and cabling");
+        }
+    }
 };
+
+// function to print the temperature for a device
+float SensorDallasTemp::printTemperature()
+{
+  sensors->requestTemperatures(); // Send the command to get temperatures
+  for(int i=0;i<numberOfDevices; i++)
+  {
+    if(sensors->getAddress(tempDeviceAddress, i))
+	{
+		Serial.print("Temperature for device: ");
+        printAddress(tempDeviceAddress);
+        Serial.println(":");
+		
+        float tempC = sensors->getTempC(tempDeviceAddress);
+        Serial.print("Temp C: ");
+        Serial.println(tempC);
+        return tempC;
+    } 
+	//else ghost device! Check your power requirements and cabling
+	
+  }
+}
